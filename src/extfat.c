fe0c29e6c4c6a8fd02405f540919f3ea278a4565
Hoai Dinh <104734625+hoaihdinh@users.noreply.github.com>
2023-04-10 19:08:38 +0000

    merged Andrew's iteration 2

--- src/extfat.c
+++ src/extfat.c
@@ -16,19 +16,19 @@ int main(int argc, char *argv[])
       return EXIT_FAILURE;
    }
 
-   fileInfo A = initFileInfoStruct(arguments.inFile);
+   fileInfo inputFileInfo = initFileInfoStruct(arguments.inFile);
 
-   if(arguments.flags[0] == true)
+   if(arguments.flags[help] == true)
    {
       printHelp();
    }
-   else if(arguments.flags[1] == true)
+   else if(arguments.flags[copy] == true)
    {
       mmapCopy(arguments.inFile, arguments.outFile);
    }
-   else if(arguments.flags[2] == true)
+   else if(arguments.flags[verify] == true)
    {
-      int stat = verifyBoot(A.mainBoot, A.backupBoot);
+      int stat = verifyBoot(inputFileInfo.mainBoot, inputFileInfo.backupBoot);
       if(stat == 0)
          printf("Main Boot and Backup Boot are the same.\nstat = 0\n");
       else
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
          printf("Main Boot and Backup Boot are not the same.\nstat = 1\n");
       }
    }
-   else if(arguments.flags[3] == true)
+   else if(arguments.flags[printDirectory] == true)
    {
       printf("Print directory function has not been implemented yet\n");
       // Call the directory function (to be implemented in a later iteration)
