d88e9e4911bf85452319c904dd4831170cf1bbf7
Hoai Dinh <104734625+hoaihdinh@users.noreply.github.com>
2023-04-28 04:57:53 +0000

    Modified the findFileAndDirEntry() function
    - Changed it to where it prioritizes finding a non directory
    target file, even if it finds a directory with the correct name
    - If it cannot find a target file with the correct name, then
    it will return the last directory found with the same name
    - Removed the Found printf in the function

--- include/searchExtfat.h
+++ include/searchExtfat.h
@@ -12,7 +12,14 @@ void *findCluster(int N, void *fp, ClusterInfo c);
  * dest should have enough space to store lengthOfName characters +1 for '\0'. */
 void fetchNameFromExtFAT(char *dest, char *ptrToFilename, int lengthOfName);
 
-/* Finds the FileAndDirEntry (Entry Type = 0x85) of a target file/directory (fileToFind) */
+/* Finds the FileAndDirEntry (Entry Type = 0x85) of a target file/directory (fileToFind) 
+ * Returns NULL if there is no file or directory with the target file name.
+ *
+ * If the file with the target name found is a directory, continue searching until a 
+ * non-directory file with the target is found. If there is none, then return the directory.
+ * 
+ * If there are multiple files with the target name, then return the first non-directory 
+ * file encountered. */
 GDS_t *findFileAndDirEntry(GDS_t *GDS, char *fileToFind, void *fp, ClusterInfo clustInfo);
 
 /* Finds the Allocation BitMap Location in the exFAT image file */
--- src/searchExtfat.c
+++ src/searchExtfat.c
@@ -29,17 +29,30 @@ void fetchNameFromExtFAT(char *dest, char *ptrToFilename, int lengthOfName)
    *dest = '\0'; // Writes '\0' to the end of the string
 }
 
-/* Finds the FileAndDirEntry (Entry Type = 0x85) of a target file/directory (fileToFind) */
-GDS_t *findFileAndDirEntry(GDS_t *GDS, char *fileToFind, void *fp, ClusterInfo clustInfo)
+/* Finds the FileAndDirEntry (Entry Type = 0x85) of a target file/directory (targetFile) 
+ * Returns NULL if there is no file or directory with the target file name.
+ *
+ * If the file with the target name found is a directory, continue searching until a 
+ * non-directory file with the target is found. If there is none, then return the directory.
+ * 
+ * If there are multiple files with the target name, then return the first non-directory 
+ * file encountered. */
+GDS_t *findFileAndDirEntry(GDS_t *GDS, char *targetFile, void *fp, ClusterInfo clustInfo)
 {
-   GDS_t *returnVal = NULL; // Defaulted to NULL, so if a file is not found, NULL is returned.
+   // The intended value to return. Is not NULL when a non-directory file of the target is found
+   GDS_t *returnVal = NULL;
+
+   // The backup value to return. Is not NULL when a directory file of the target is found
+   GDS_t *backupVal = NULL;
+
    int i = 0;
-   // While loop keeps running until the target file is found or until there are no more entries.
+   /* While loop keeps running until the target file is found and 
+    * it is not a directory, or until there are no more entries. */
    while(GDS[i].EntryType && returnVal == NULL)
    {
-      // Checks if if current GDS is FileAndDirectoryEntry (FileDir - 0x85)
-      // and the next GenericDirectoryStructure (i+1) is a StreamExtensionEntry (StreamExt - 0xc0)
-      // and the one after that (i+2) is the FileNamEntry (FileName - 0xc1)
+      /* Checks if if current GDS is FileAndDirectoryEntry (FileDir - 0x85)
+       * and the next GenericDirectoryStructure (i+1) is a StreamExtensionEntry (StreamExt - 0xc0)
+       * and the one after that (i+2) is the FileNamEntry (FileName - 0xc1) */
       if(GDS[i].InUse && GDS[i].EntryType == FileDir && GDS[i+1].EntryType == StreamExt && GDS[i+2].EntryType == FileName)
       {
          FileAttributes *fileAttributes = (FileAttributes *)( (void *)&GDS[i] + FILE_ATTRIBUTE_OFFSET );
@@ -50,18 +63,29 @@ GDS_t *findFileAndDirEntry(GDS_t *GDS, char *fileToFind, void *fp, ClusterInfo c
          char *currFilename = malloc((streamExtEntry->NameLength + 1) * sizeof(char));
          fetchNameFromExtFAT(currFilename, (char *)fileNameEntry->FileName, streamExtEntry->NameLength);
 
-         /* Checks to see if the filename is equal to the target file
-          * Else if a directory is encountered, recusively call this function to search through that directory
-          * for the target file. */
-         if(strcmp(currFilename, fileToFind) == 0)
+         // Checks to see if the filename is equal to the targetFile
+         if(strcmp(currFilename, targetFile) == 0)
          {
-            printf("Found %s.\n", currFilename);
-            returnVal = &GDS[i];
+            /* If the file is a directory, recursively call this function to see if a
+             * non-directory file with the targetName exists */
+            if(fileAttributes->Directory)
+            {
+               backupVal = &GDS[i]; // Found a directory version of the file, set up backupVal
+               GDS_t *subGDS = findCluster(streamExtEntry->FirstCluster, fp, clustInfo);
+               returnVal = findFileAndDirEntry(subGDS, targetFile, fp, clustInfo);  
+            }
+            else // The file is not a directory, and prepare to leave the loop
+            {
+               returnVal = &GDS[i];
+            }
          }
-         else if(fileAttributes->Directory)
+         
+         /* If a directory is encountered, recusively call this function to 
+          * search through that directory for the target file. */
+         if(fileAttributes->Directory)
          {
             GDS_t *subGDS = findCluster(streamExtEntry->FirstCluster, fp, clustInfo);
-            returnVal = findFileAndDirEntry(subGDS, fileToFind, fp, clustInfo);      
+            returnVal = findFileAndDirEntry(subGDS, targetFile, fp, clustInfo);      
          }
 
          free(currFilename);
@@ -70,7 +94,9 @@ GDS_t *findFileAndDirEntry(GDS_t *GDS, char *fileToFind, void *fp, ClusterInfo c
       i++;
    }
 
-   return returnVal;
+   /* If returnVal is NULL, then a non-directory of the targetFile was not found, so
+    * return the backupVal. If backupVal is NULL, then the targetFile did not exist. */
+   return (returnVal) ? returnVal : backupVal;
 }
 
 /* Finds the Allocation BitMap Location in the exFAT image file */
